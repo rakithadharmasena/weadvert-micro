c2967e6a3d1f61f886f5cfb0230656cf6c3074f9
rakithadharmasena <rakithadharmasena@gmail.com>
2020-07-03 01:54:41 +0530

    adding SignUp page

--- WebAdvert.Web/Controllers/AccountsController.cs
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -2,14 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models.Accounts;
 
 namespace WebAdvert.Web.Controllers
 {
     public class AccountsController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager,UserManager<CognitoUser> userManager,CognitoUserPool pool)
         {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+            this._pool = pool;
+        }
+
+        public async Task<IActionResult> SignUp()
+        {
+            var model = new SignUpModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.ToString(), model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
+
             return View();
         }
     }
--- WebAdvert.Web/Startup.cs
+++ WebAdvert.Web/Startup.cs
@@ -22,6 +22,7 @@ namespace WebAdvert.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCognitoIdentity();
             services.AddControllersWithViews();
         }
 
@@ -39,7 +40,7 @@ namespace WebAdvert.Web
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

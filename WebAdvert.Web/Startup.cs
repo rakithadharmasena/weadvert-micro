7c5490acc00cb2b70ca856b75d1b800ba246810f
rakithadharmasena <rakithadharmasena@gmail.com>
2020-07-21 00:55:46 +0530

    adding advert management

--- WebAdvert.Web/Startup.cs
+++ WebAdvert.Web/Startup.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebAdvert.Web.Services;
+using AutoMapper;
+using WebAdvert.Web.ServiceClients;
+using Polly;
+using System.Net.Http;
+using Polly.Extensions.Http;
+using System.Net;
 
 namespace WebAdvert.Web
 {
@@ -22,12 +29,29 @@ namespace WebAdvert.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddCognitoIdentity();
             services.ConfigureApplicationCookie((options)=>
             {
                 options.LoginPath = "/Accounts/Login";
             });
+            //S3
+            services.AddTransient<IFileUploader, S3FileUploader>();
+            services.AddHttpClient<IAdvertApiClient, AdvertApiClient>().AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(GetCircuitBreakerPatternPolicy());
             services.AddControllersWithViews();
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPatternPolicy()
+        {
+            return HttpPolicyExtensions.HandleTransientHttpError().CircuitBreakerAsync(3, TimeSpan.FromSeconds(30));
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions.HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(5, retryAttempy => TimeSpan.FromSeconds(Math.Pow(2, retryAttempy)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
